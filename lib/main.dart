96e63291a81aef9e03e5d985e1b16939b8da46b3
VolkovRV <masha011992@bk.ru>
2022-02-16 11:02:41 +0300

    Получение и отображение данных из сети

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:untitled1/screens/app_bar_screen.dart';
 import 'package:untitled1/screens/file_demo_screen.dart';
 import 'package:untitled1/screens/listview_screen.dart';
 import 'package:untitled1/screens/navigation_drawer_screen.dart';
+import 'package:untitled1/screens/networking_screen.dart';
 import 'package:untitled1/screens/shared_perf_screen.dart';
 import 'package:untitled1/screens/shared_pref_file_screen.dart';
 import 'package:untitled1/screens/switches_demo_screen.dart';
@@ -11,5 +12,5 @@ import 'screens/text-demo-screen.dart';
 
 
 void main() async {
-  runApp(const DemoScreen());
+  runApp(const NetWorkingScreen());
 }
---
+++ lib/screens/networking_screen.dart
@@ -0,0 +1,90 @@
+import 'dart:async';
+import 'dart:convert';
+
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+
+Future<Post> fetchPost() async {
+  final response = await http
+      .get(Uri.parse('https://jsonplaceholder.typicode.com/posts/1'));
+
+  if (response.statusCode == 200) {
+    // If the server did return a 200 OK response,
+    // then parse the JSON.
+    return Post.fromJson(jsonDecode(response.body));
+  } else {
+    // If the server did not return a 200 OK response,
+    // then throw an exception.
+    throw Exception('Failed to load post');
+  }
+}
+
+class Post {
+  final int userId;
+  final int id;
+  final String title;
+  final String body;
+
+  const Post({
+    required this.userId,
+    required this.id,
+    required this.title,
+    required this.body,
+  });
+
+  factory Post.fromJson(Map<String, dynamic> json) {
+    return Post(
+      userId: json['userId'],
+      id: json['id'],
+      title: json['title'],
+      body: json['body'],
+    );
+  }
+}
+
+class NetWorkingScreen extends StatefulWidget {
+  const NetWorkingScreen({Key? key}) : super(key: key);
+
+  @override
+  _NetWorkingScreenState createState() => _NetWorkingScreenState();
+}
+
+class _NetWorkingScreenState extends State<NetWorkingScreen> {
+  late Future<Post> futurePost;
+
+  @override
+  void initState() {
+    super.initState();
+    futurePost = fetchPost();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Fetch Data Example',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Fetch Data Example'),
+        ),
+        body: Center(
+          child: FutureBuilder<Post>(
+            future: futurePost,
+            builder: (context, snapshot) {
+              if (snapshot.hasData) {
+                return Text(snapshot.data!.body);
+              } else if (snapshot.hasError) {
+                return Text('${snapshot.error}');
+              }
+
+              // By default, show a loading spinner.
+              return const CircularProgressIndicator();
+            },
+          ),
+        ),
+      ),
+    );
+  }
+}
